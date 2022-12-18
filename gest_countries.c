7d92e0c6876bd708f46c9bf3b8bf7755bbd1c954
luis.damas@estsetubal.ips.pt <luis.damas@estsetubal.ips.pt>
2022-12-18 15:21:36 +0000

    Changed sort function

--- gest_countries.c
+++ gest_countries.c
@@ -8,7 +8,7 @@
 
 #define MAX_CODE2_SIZE 2
 #define MAX_CODE3_SIZE 3
-#define MAX_COUNTRY_SIZE 50
+#define MAX_COUNTRY_SIZE 100
 
 #define MAX_COUNTRIES 100
 
@@ -37,7 +37,7 @@ void manage_countries(){
   char aux[MAX_STRING];
 
 
-  wait("Importar o ficheiro csv com os paises. Prima <ENTER>...");
+  wait("Importar o ficheiro csv com os paises. Prima <ENTER>...\n");
   // Importar os dados do ficheiro para a lista de countries
   FILE *fp;         // Ficheiro para importar os dados.
   sprintf(aux, "%s/%s", "datasets", "countries.csv");
@@ -75,8 +75,10 @@ void manage_countries(){
   }
 
 
-  puts("\n\nLimpar a lista de registos em memoria."); list_countries.count=0; // Lista vazia.
-  wait("Ler a lista de registos a partir do ficheiro binario. Prima <ENTER>...");
+  puts("\n\nLimpar a lista de registos em memoria."); 
+  list_countries.count=0; // Lista vazia.
+ 
+ wait("Ler a lista de registos a partir do ficheiro binario. Prima <ENTER>...");
 
   if ((fp_country = open_data_file(DIR_DATA, DATA_FILE_COUNTRY))==NULL)      // Abrir o ficheiro de dados
     return ;
@@ -102,28 +104,28 @@ void manage_countries(){
 
 
 
-  puts("\n\nProcura binaria do pais {PT POR Portugal} na lista ordenada de paises.");
+  puts("\n\nProcura binaria do pais [Portugal] na lista ordenada de paises.");
   COUNTRY p = {"PT","POR","Portugal"};
   COUNTRY *ptr = bsearch(&p, list_countries.arr, (long) list_countries.count, sizeof(COUNTRY), compare);
   if (ptr!=NULL)
   {
     show_country(*ptr);
-    wait("Encontrado o pais PT POR Portugal\n\n");
+    wait("Encontrado o pais [Portugal]\n\n");
   }
   else
-    wait("NAO FOI ENCONTRADO pais PT POR Portugal\n\n");
+    wait("NAO FOI ENCONTRADO pais [Portugal]\n\n");
 
 
-  puts("\n\nProcura binaria do pais {PT POR Portugallo} na lista ordenada de paises.");
+  puts("\n\nProcura binaria do pais [Portugallo] na lista ordenada de paises.");
   strcat(p.country,"lo"); // Juntar "lo" a "Portugal" para se obter "Portugallo"
   ptr = bsearch(&p, list_countries.arr, (long) list_countries.count, sizeof(COUNTRY), compare);
   if (ptr!=NULL)
   {
     show_country(*ptr);
-    wait("Encontrado o pais PT POR Portugallo\n\n");
+    wait("Encontrado o pais [Portugallo]\n\n");
   }
   else
-    wait("NAO FOI ENCONTRADO pais PT POR Portugallo\n\n");
+    wait("NAO FOI ENCONTRADO pais [Portugallo]\n\n");
 
   wait("That's all!!!");
 }
@@ -152,5 +154,18 @@ void show_countries(LIST_COUNTRIES list){
 int compare(const void*ptr1, const void* ptr2){
   COUNTRY *p1 = (COUNTRY *) ptr1;
   COUNTRY *p2 = (COUNTRY *) ptr2;
+
+/*
+  // comparar code2
+  int res = strcmp(p1->code2, p2->code2);
+  if (res!=0) return res;
+  
+
+  // comparar code3
+  res = strcmp(p1->code3, p2->code3);
+  if (res!=0) return res;
+*/
+
+  // comparar o descritivo do pais
   return strcmp(p1->country, p2->country);
 }
